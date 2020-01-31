cd5a232ca3073cc345a8b5b7da4a86ea66292ed7
ski lee <540470452@qq.com>
2020-01-31 16:22:27 +0800

    添加项目文件。

---
+++ MyVideo/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Tools;
+
+namespace MyVideo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> Efiles = new List<string>();
+            FileTools.GetFilesByPathAndExt(ref Efiles,@"E:\HD","*.mp4");
+            ViewBag.Efiles = Efiles;
+
+            List<string> Ffiles = new List<string>();
+            FileTools.GetFilesByPathAndExt(ref Ffiles, @"F:\电影", "*.mp4");
+            ViewBag.Ffiles = Ffiles;
+
+            List<string> Gfiles = new List<string>();
+            FileTools.GetFilesByPathAndExt(ref Gfiles, @"G:\电影", "*.mp4");
+            ViewBag.Gfiles = Gfiles;
+
+            return View();
+        }
+
+        public ActionResult Movie(string id)
+        {
+            ViewBag.Message = id;
+
+            ViewBag.MovieName = id;
+
+            string physicEPath = @"E:\HD";
+            string physicFPath = @"F:\电影";
+            string physicGPath = @"G:\电影";
+
+            string movieNameE = string.Empty;
+            FileTools.GetFileFullPathByfileNameAndParentPath(physicEPath, id, ref movieNameE);
+            string movieNameF = string.Empty;
+            FileTools.GetFileFullPathByfileNameAndParentPath(physicFPath, id, ref movieNameF);
+            string movieNameG = string.Empty;
+            FileTools.GetFileFullPathByfileNameAndParentPath(physicGPath, id, ref movieNameG);
+
+            if (movieNameE.Contains(physicEPath))
+            {
+                ViewBag.movieSubPath = "/EVideo/" + movieNameE.Substring(physicEPath.Length);
+            }
+            else if (movieNameF.Contains(physicFPath))
+            {
+                ViewBag.movieSubPath = "/FVideo/" + movieNameF.Substring(physicFPath.Length);
+            }
+            else if (movieNameG.Contains(physicGPath))
+            {
+                ViewBag.movieSubPath = "/GVideo/" + movieNameG.Substring(physicGPath.Length);
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Tools/FileTools.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public static class FileTools
+    {
+        public static void GetFilesByPathAndExt(ref List<string> res,string path,string ext="*")
+        {           
+            DirectoryInfo theFolder = new DirectoryInfo(@path);
+
+            //遍历文件
+            foreach (FileInfo NextFile in theFolder.GetFiles(ext))
+            {
+                res.Add(Path.GetFileName(NextFile.FullName));
+            }
+
+            //遍历文件夹
+            foreach (DirectoryInfo NextFolder in theFolder.GetDirectories())
+            {
+                GetFilesByPathAndExt(ref res, NextFolder.FullName,ext);
+            }
+        }
+        public static void GetFileFullPathByfileNameAndParentPath(string parentPath,string fileName,ref string resFileFullName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(parentPath);
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (var file in files)
+            {
+                string name = Path.GetFileName(file.FullName);
+                if(name.Equals(fileName))
+                {
+                    resFileFullName = file.FullName;
+                    return;
+                }
+            }
+            foreach (var subDir in dirs)
+            {
+                if (string.IsNullOrEmpty(resFileFullName))
+                {
+                    GetFileFullPathByfileNameAndParentPath(subDir.FullName, fileName, ref resFileFullName);
+                }
+            }          
+        }
+
+    }
+}
