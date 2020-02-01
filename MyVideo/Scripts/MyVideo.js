0c760bf7c81007fe781282e20d5efb603eff0853
ski lee <540470452@qq.com>
2020-02-01 17:58:10 +0800

    增加视屏变速和暂停播放按钮js

---
+++ MyVideo/Scripts/MyVideo.js
@@ -0,0 +1,20 @@
+﻿var video = document.getElementById("mainVideo");
+var btn_play = document.getElementById("btnPlay");
+video.addEventListener("pause", function () {
+    btn_play.innerHTML = "播放";
+})
+video.addEventListener("play", function () {   
+    btn_play.innerHTML = "暂停";
+})
+
+btn_play.addEventListener("click",function () {
+    if (btn_play.innerHTML == "播放")
+        video.play();
+    else
+        video.pause();
+});
+
+var selRate = document.getElementById("selRate");
+selRate.addEventListener("change", function () {
+    video.playbackRate = this.value;
+})
